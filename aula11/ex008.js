c7a17035fd2c83e824c936f97ac5bdb1cafaa292
Thiago de Souza Rodrigues <rodriguessr02@gmail.com>
2022-09-20 16:11:05 -0300

    ATT

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
-console.log('Funcionou perfeitamente')
-console.log('nao funfo nada sa merda')
+var vel = 55.5
+console.log(`A Velocidade do seu carro é ${vel} KM/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+    console.log(`Dirija sempre usando cinto de segurança`)
